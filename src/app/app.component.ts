a03d76aee4703ad0e5d20195d6c73f15949ba915
Tommy Gaessler <tommy.gaessler@gmail.com>
2017-03-21 22:52:53 -0600

    added email signup form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 declare var $:any;
 
@@ -9,11 +10,17 @@ declare var $:any;
 })
 export class AppComponent {
 
-  constructor() {
+  email: string = '';
+  response: string;
+  error: string;
+  loading: boolean = false;
+
+  constructor(private http: Http) {
 
   }
 
   ngOnInit() {
+
     var header = $('.backgound-image');
     var range = 200;
 
@@ -35,5 +42,20 @@ export class AppComponent {
         }
       }
     });
+  }
+
+  reserve() {
+
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(emailRegex.test(this.email)) {
+      this.loading = true;
+      this.http.post(`https://ujj9l4tjj6.execute-api.us-east-1.amazonaws.com/prod/give-and-go-film-reserve`, {email: this.email, FNAME: null, LNAME: null}).map((response:Response) => response.json()).subscribe((response) => {
+        this.response = response.body;
+      }, (error) => {
+        this.loading = false;
+        this.error = error.body;
+      });
+    }
   }
 }
