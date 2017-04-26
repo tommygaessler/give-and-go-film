44f678c8c8b75678d0248ca5a250d475bc5a5f80
Tommy Gaessler <tommy.gaessler@gmail.com>
2017-04-25 18:54:48 -0600

    pushing to private repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
 import { RouterModule, Routes} from '@angular/router';
 import 'hammerjs';
 
@@ -16,7 +15,6 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot(),
     ReactiveFormsModule
   ],
   providers: [],
